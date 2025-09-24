1407f28f00ca3290f6237da71a6e1b7a33ba2b12
Myriam <myriamalbustany@gmail.com>
2025-09-24 18:16:03 +0300

    fixed typescript/linter errors

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -33,6 +33,7 @@ export const Timeline = ({
     <div
       className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10"
       ref={containerRef}>
+
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
 
         <h2 className="text-5xl md:text-4xl lg:text-6xl mb-4 text-black font-bold dark:text-white max-w-4xl">
